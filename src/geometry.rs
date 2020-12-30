d5355ec7d0992a2b58668cfc6c8cd37a10bb624a
Simone <kill.krt@gmail.com>
2020-12-30 11:52:11 +0100

    Adds Terrain

--- src/geometry.rs
+++ src/geometry.rs
@@ -2,3 +2,6 @@
 pub mod vector;
 // Imports Volume
 pub mod volume;
+
+// Imports CellType, CellMaterial, Terrain
+pub mod terrain;
---
+++ src/geometry/terrain.rs
@@ -0,0 +1,57 @@
+use std::collections::HashMap;
+
+use log::warn;
+
+use super::{vector::Position, volume::Volume};
+
+/// Possible types of cell materials
+#[derive(Debug, Copy, Clone)]
+enum CellMaterial {
+    Water,
+    Ground,
+}
+
+/// Possible types of cell
+#[derive(Debug, Copy, Clone)]
+enum CellType {
+    /// Cell full of material (cannot placed anything on top of that)
+    Fill(CellMaterial),
+    /// Flat cell (stuff can be placed on top of that)
+    Flat(CellMaterial),
+}
+
+/// Represents a terrain where players can place their pawns
+///
+/// Terrain has a volume (its bounding box), cells can be placed only
+/// inside this volume.
+#[derive(Debug)]
+pub struct Terrain {
+    /// Bounding box
+    volume: Volume,
+    /// Maps that for each valid position returns the type of cell at that position.
+    cells: HashMap<Position, CellType>,
+}
+
+impl Terrain {
+    /// Create an empty terrain with provided volume.
+    ///
+    /// The terrain doesn't contain any cells.
+    pub fn new(volume: &Volume) -> Self {
+        Self {
+            volume: *volume,
+            cells: HashMap::new(),
+        }
+    }
+
+    /// Get cells type at selected position
+    ///
+    /// Returns `None` if position is not valid (outside the volume) or
+    /// position is empty
+    pub fn get_at(&self, position: &Position) -> Option<CellType> {
+        if !self.volume.is_inside(position) {
+            warn!("Position {:?} is outside of terrain", position);
+            return None;
+        }
+        self.cells.get(position).copied()
+    }
+}

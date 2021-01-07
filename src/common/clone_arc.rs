24d1c577c1b6ddcda3ec8432b1cd38d2b4fa69f6
Kill KRT <kill.krt@gmail.com>
2021-01-07 21:29:56 +0100

    Fixes liftetime for CloneArc

--- src/common/clone_arc.rs
+++ src/common/clone_arc.rs
@@ -6,12 +6,17 @@ pub trait Clonable {}
 /// Trait that provide a method to clone via `Arc`
 pub trait CloneArc {
     /// Returns an Arc that contains the clone of this object
-    fn clone_arc<'slf>(&'slf self) -> Arc<dyn Clonable + 'slf>;
+    fn clone_arc<'slf>(self: &'_ Self) -> Arc<dyn Clonable + 'slf>
+    where
+        Self: 'slf;
 }
 
 impl<T: Clone + Clonable> CloneArc for T {
     /// Returns an Arc that contains the clone of this object
-    fn clone_arc<'slf>(&'slf self) -> Arc<dyn Clonable + 'slf> {
+    fn clone_arc<'slf>(self: &'_ Self) -> Arc<dyn Clonable + 'slf>
+    where
+        Self: 'slf,
+    {
         Arc::new(self.clone())
     }
 }

9da7e9fd94471285fab0dd8397179a24307cfeb1
Kill KRT <kill.krt@gmail.com>
2021-01-15 00:57:48 +0100

    Adds little docs

--- src/common/clone_arc.rs
+++ src/common/clone_arc.rs
@@ -1,12 +1,15 @@
 use std::sync::Arc;
 
+/// Generic clonable object
 pub trait Clonable {}
 
+/// Fake trait
 pub trait ArcClone<T> {
     fn arc_clone(&self) -> Arc<T>;
 }
 
 impl<T: Clone + Clonable> ArcClone<T> for T {
+    /// Implementation for `ArcClone` trait for clonable object
     fn arc_clone(&self) -> Arc<T> {
         Arc::new(self.clone())
     }
